249b507c052f348c1864c40d2da1b85dc1100b72
rpdjf <ruipaulo.unify@outlook.fr>
2024-01-23 06:26:05 +0100

    some good work

--- src/philosophers.c
+++ src/philosophers.c
@@ -6,18 +6,22 @@
 /*   By: rude-jes <rude-jes@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 02:25:08 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/01/23 05:56:01 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/01/23 06:25:48 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+pthread_mutex_t	mutex;
+
 static void	*start_routine(void *param)
 {
 	t_philosopher	*philosopher;
 
+	pthread_mutex_lock(&mutex);
 	philosopher = (t_philosopher *)param;
 	ft_printf("Je suis le %dième philosopher !\n", philosopher->id);
+	pthread_mutex_unlock(&mutex);
 	return (0);
 }
 
@@ -27,15 +31,22 @@ static int	start_threads(t_philosopher **philosophers)
 	{
 		if (pthread_create(&(*philosophers)->thread, NULL, start_routine, *philosophers))
 			return (-1);
-		(*philosophers)++;
+		philosophers++;
 	}
 	return (0);
 }
 
 static int	start_philo(t_data *data)
 {
+	t_philosopher	**philosophers;
+
+	pthread_mutex_init(&mutex, 0);
 	if (start_threads(data->philosophers) < 0)
 		return (-1);
+	philosophers = data->philosophers;
+	while (*philosophers)
+		pthread_join((*(philosophers++))->thread, 0);
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
 
@@ -50,6 +61,5 @@ int	main(int argc, char **argv)
 		return (crash_exit());
 	if (start_philo(data) < 0)
 		return (crash_exit());
-	start_philo(data);
 	return (secure_exit());
 }
